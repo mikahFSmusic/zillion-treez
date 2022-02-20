babe32e395389d031f5f8b3e9a6fabb3fff4cc15
Mikah <mikahfsmusic@gmail.com>
2022-02-20 00:08:02 -0500

    adds linting rules and some text

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Background } from './modules/background';
+import { Background } from './modules/landingPage/background';
 
 function App() {
   return (
---
+++ front/src/modules/landingPage/textContent/TextContent.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@material-ui/core';
+
+const TextContent = () => {
+  return (
+    <>
+      <Typography variant="h1">Coming Soon...</Typography>
+      <Typography variant="h3">
+        Bottom text: You buy an NFT, we plant a tree!
+      </Typography>
+    </>
+  );
+};
+
+export default TextContent;
